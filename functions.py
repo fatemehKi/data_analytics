af12f86dba3e4d49d73b369e70f9d6e1362dae45
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-03-22 13:36:13 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -22,6 +22,10 @@ not indent is showing the new part
 unless you save it in an specif variables.. i.e., if we put the return in the specific variables it will 
 - unlike C++ if we assign the output as a variable; however, we don't have a return, we don't get error; however, we don't get anything
 - in orfr to get the documentation inside a function (first line after the def ) we use print(foo.__doc__)
+- the inner function can not be calledr out of the outer function and when we call the outer function the inner func
+- Lambda function is similar to the inlije finction and we have the 'Lambda' keyword.. it does do the print as well:
+    foo = lambda arg1, arg2: functionlity
+- lambda function insied the function requires attention for calling
 '''
 
 def print_me(name, age):
@@ -77,5 +81,76 @@ z
 print(sumxy1.__doc__)
 print(max.__doc__)
 
+# we created confusion on y ######## we can't have different name for a and b###### and c is considered a global variable for the internal function
+def outfunc(a,b):
+    x=a+b
+    c =1
+    def infunc(x,y):
+        return(x-y+c)
+   y= infunc(a,b)
+   return(x*y)
+print 
 
+#the variables in the outer functions are like the global variables for the inner function;
+#we are unable to call the inner function out of the outer function
+def outer (num1):
+    x=10
+    def inner_increament(num1):
+        return num1+x
+    num2 = inner_increment(num1)
+    print(num1, num2)
+
+#local vs global variables
+def outer (num1):
+    x=10
+    def inner_increament(num3):
+        return num1+x
+    num2 = inner_increament(num1)
+    print(num1, num2)
+
+outer(10)
+
+##### scalling.. how to pass a list of values rather that scaled value in the function
+
+def stdrange(inputlist):
+    result = inputlist[:]
+    print("the original list"+str(result))
+    for j in range(0,11):
+        result[j]=((inputlist[j])-min(inputlist))/(max(inputlist)-min(inputlist))
+        #result[j]=(float(inputlist[j])-min(inputlist))/(max(inputlist)-min(inputlist)) this is correct too
+    print ('the new list is ' + str(result))
+    return(result)
+
+x= stdrange(list(range(1,12)))
+x=stdrange([1.2, 2.3, 4.0, 4.5, 3.1, 1.7, 2.2, 3.4, 5.0, 9.0, 7.4])
+
+####lambda function
+a = lambda r:3.14*r*r 
+a(5.2)
+#with two args
+b = lambda x,y:x+y
+b(1,10)
+
+#arguments can be passed directly
+def PrintMax(a,b):
+    if a>b:
+        print(a, 'is Max')
+    elif a==b:
+        print(a, 'is equal to ', b )
+    else:
+        print(b, ' is maximum')
+
+PrintMax(3,4)
+
+# Lambda function inside the 
+def myfunc(n):
+    return lambda a : a * n
+
+#mydoubler is working like lambda function
+
+mydoubler = myfunc(2)
+mydoubler
+mydoubler(11)
+
+ 
 
